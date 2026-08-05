1cf405bbe3f270371ed085de69e3f52dc4d43c5b
Anna Maria <annamaria.academic@gmail.com>
2026-08-05 20:10:13 +0530

    Palindrome Number

---
+++ Palindrome_Number.py
@@ -0,0 +1,23 @@
+#Question: Given an integer x, return true if x is a palindrome, and false otherwise.
+
+#Example 1:
+
+#Input: x = 121
+#Output: true
+#Explanation: 121 reads as 121 from left to right and from right to left.
+
+def isPalindrome(x):
+        x = str(x)
+        y = x[::-1]
+        if x == y:
+            return True
+        else:
+            return False
+
+print(isPalindrome(101))
+
+#Follow Up question: Could you solve it without converting the integer to a string?
+
+
+
+
